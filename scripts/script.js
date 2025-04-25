598dc02cbdf3a897d9142730f8323dfff51830fd
Christian <christian.mig.flmo@gmail.com>
2025-04-25 09:09:40 -0600

    Adding custom error messages for input

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,11 @@
 
 const subscribeButton = document.querySelector('.btn.signup__info-submit');
 const dismissButton = document.querySelector('.btn.dismiss-btn');
-
 const components = document.querySelectorAll('.component');
 const form = document.querySelector('.signup__info-form');
 const inputEmail = document.getElementById('input-email');
-
 const userEmailTxt = document.querySelector('.user-email');
+const bubbleError = document.querySelector('.signup__info-email-error');
 
 init();
 
@@ -17,19 +16,18 @@ function init() {
 
 	/* Setup event listeners */
 	form.addEventListener('submit', formListener);
-	dismissButton.addEventListener('click', formListener);
+	dismissButton.addEventListener('click', dismissListener);
 	inputEmail.addEventListener('input', emailValidationListener);
-
-	/* Set initial empty value error */
-	if (inputEmail.validity.valueMissing) {
-		inputEmail.setCustomValidity("Please enter an email address here");
-	}
 }
-
 
 /* ================= Listener callbacks ================= */
 function formListener(e) {
 	e.preventDefault();
+
+	if (!inputEmail.validity.valid) {
+		displayError();
+		return;
+	}
 
 	components.forEach(component => component.classList.toggle('hidden'));
 	userEmailTxt.textContent = inputEmail.value;
@@ -42,15 +40,29 @@ function formListener(e) {
 function emailValidationListener(e) {
 	e.preventDefault();
 
-	if (inputEmail.validity.valueMissing) {
-		inputEmail.setCustomValidity("Please enter an email address here");
-	} else {
-		inputEmail.setCustomValidity(inputEmail.validity.typeMismatch ? "Input a valid email address" : "");
-	}
-
 	if (inputEmail.checkValidity()) {
 		subscribeButton.classList.add('valid-data');
+		bubbleError.classList.add('hidden-error');
+		bubbleError.classList.remove('fade-in');
 	} else {
 		subscribeButton.classList.remove('valid-data');
+		displayError();
 	}
+}
+
+function dismissListener(e) {
+	e.preventDefault();
+	components.forEach(component => component.classList.toggle('hidden'));
+}
+
+/* ================= Utility functions ================= */
+function displayError() {
+	if (inputEmail.validity.valueMissing) {
+		bubbleError.textContent = 'Please, enter an email address';
+	} else if (inputEmail.validity.typeMismatch) {
+		bubbleError.textContent = 'Email address needs to be a valid format';
+	}
+
+	bubbleError.classList.remove('hidden-error');
+	bubbleError.classList.add('fade-in');
 }
